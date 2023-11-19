dba57ef410922f0bf10dfea81b4ed86ef9fc4c25
sm4z0n3 <smazone@outlook.com>
2023-11-19 16:43:54 +0800

    更新通知显示功能窗体

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -45,46 +45,65 @@ namespace 灵动窗Framework
             notifyIcon = new NotifyIcon
             {
                 Icon = SystemIcons.Application,
-                Visible = true
+                Visible = true,
+                Text = "Nova灵动窗 | 单击打开设置 | 双击关闭程序"
             };
             notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+            notifyIcon.MouseClick += NotifyIcon_Click;
         }
 
         private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+        private void NotifyIcon_Click(object sender, EventArgs e)
         {
             Show();
             WindowState = WindowState.Normal;
         }
         private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            e.Cancel = true; // 阻止窗口关闭
-
-            // 隐藏主窗口，而不是关闭
+            e.Cancel = true;
             Hide();
         }
-
         private void Window_Width_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             widthA = this.Window_Width.Value;
             isLandWindow.SetWidth(widthA);
         }
-
         private void Window_Radius_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             radiusA = this.Window_Radius.Value;
             isLandWindow.SetRadius(radiusA);
         }
-
         private void Window_Height_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             heightA = this.Window_Height.Value;
             isLandWindow.SetHeight(heightA);
         }
-
         private void Window_Top_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             topA = this.Window_Top.Value;
             isLandWindow.SetTop(topA);
+        }
+
+        private async void Test_Content_Click(object sender, RoutedEventArgs e)
+        {
+           await isLandWindow.ShowContent(
+               0.6,
+               5,
+               "你好世界",
+               new System.Windows.Controls.Image(),
+               Brushes.White
+               );
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Window_Width.Value = 120;
+            Window_Height.Value = 35;
+            Window_Radius.Value = 15;
+            Window_Top.Value = 20;
         }
     }
 
--- isLand.xaml.cs
+++ isLand.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +14,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace 灵动窗Framework
 {
@@ -24,6 +26,7 @@ namespace 灵动窗Framework
         private DoubleAnimation opacityAnimation;
         private DoubleAnimation widthAnimation;
 
+        bool broder_s = false;
         public isLand()
         {
             InitializeComponent();
@@ -32,6 +35,7 @@ namespace 灵动窗Framework
             BorderWindow.MouseLeave += BorderWindow_MouseLeave;
             BorderWindow.MouseRightButtonUp += BorderWindow_MouseRightButtonUp;
         }
+
         double aawidth = 120;
         public void SetHeight(double height)
         {
@@ -50,14 +54,91 @@ namespace 灵动窗Framework
         {
             this.Top = top;
         }
+
+        bool isLabelVisible = false;
+        public async Task ShowContent(double AnimationTime, int ShowTime, string body, Image icon, Brush color)
+        {
+            double oldhei = BorderWindow.Height;
+            double oldwid = BorderWindow.Width;
+            ElasticEase elasticEase = new ElasticEase
+            {
+                Oscillations = 1, // Number of oscillations
+                Springiness = 10 // The "springiness" of the animation
+            };
+            DoubleAnimation ContantHeight = new DoubleAnimation
+            {
+                Duration = TimeSpan.FromSeconds(AnimationTime),
+                From = oldhei,
+                To = 0.3,
+                EasingFunction = elasticEase // Apply the ElasticEase
+            };
+            DoubleAnimation ContantWidth = new DoubleAnimation
+            {
+                Duration = TimeSpan.FromSeconds(AnimationTime),
+                From = oldwid,
+                To = 0.3,
+                EasingFunction = elasticEase // Apply the ElasticEase
+            };
+
+            ContantHeight.To = 110;
+            ContantWidth.To = 500;
+            BorderWindow.BeginAnimation(Border.HeightProperty, ContantHeight);
+            BorderWindow.BeginAnimation(Border.WidthProperty, ContantWidth);
+            isLabelVisible = true;
+            Label newLabel = new Label
+            {
+                Content = body,
+                FontSize = 16,
+                Margin = new Thickness(20),
+                Foreground = color
+            };
+
+            BorderBody.Children.Add(newLabel);
+
+            async Task DelayedAnimation()
+            {
+                await Task.Delay(ShowTime * 1000);
+                ContantHeight.To = oldhei;
+                ContantWidth.To = oldwid;
+                ContantHeight.From = 110;
+                ContantWidth.From = 500;
+                BorderWindow.BeginAnimation(Border.HeightProperty, ContantHeight);
+                BorderWindow.BeginAnimation(Border.WidthProperty, ContantWidth);
+                BorderBody.Children.Remove(newLabel);
+                isLabelVisible = false;
+            }
+
+            var delayedTask = DelayedAnimation();
+
+            BorderWindow.MouseDown += async (sender, e) =>
+            {
+                if (isLabelVisible && e.ChangedButton == MouseButton.Left)
+                {
+                    ContantHeight.To = oldhei; // 回弹10px
+                    ContantWidth.To = oldwid; // 回弹10px
+                    ContantHeight.From = 110;
+                    ContantWidth.From = 500;
+                    BorderWindow.BeginAnimation(Border.HeightProperty, ContantHeight);
+                    BorderWindow.BeginAnimation(Border.WidthProperty, ContantWidth);
+                    BorderBody.Children.Remove(newLabel);
+                    await Task.Delay(800); // 等待0.8秒，与之前回弹动画的时间相匹配
+                    await delayedTask; // 等待原本的延迟动画完成
+                }
+            };
+
+            await delayedTask;
+        }
+
+
         private void InitializeAnimations()
         {
             opacityAnimation = new DoubleAnimation
             {
                 Duration = TimeSpan.FromSeconds(0.2),
                 From = 1.0,
-                To = 0.3
+                To = 0.5
             };
+
 
             widthAnimation = new DoubleAnimation
             {
@@ -69,32 +150,37 @@ namespace 灵动窗Framework
 
         private void BorderWindow_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            opacityAnimation.To = 0.3;
+            opacityAnimation.To = 0.5;
             opacityAnimation.From = 1.0;
             BorderWindow.BeginAnimation(Border.OpacityProperty, opacityAnimation);
         }
-
         private void BorderWindow_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
             opacityAnimation.To = 1.0;
-            opacityAnimation.From = 0.3;
+            opacityAnimation.From = 0.5;
             BorderWindow.BeginAnimation(Border.OpacityProperty, opacityAnimation);
         }
 
         private void BorderWindow_MouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            if (BorderWindow.Width == aawidth)
+            if (!isLabelVisible)
             {
-                widthAnimation.From = aawidth;
-                widthAnimation.To = 35;
-            }
-            else
-            {
-                widthAnimation.From = 35;
-                widthAnimation.To = aawidth;
+                if (BorderWindow.Width == aawidth)
+                {
+                    widthAnimation.From = aawidth;
+                    widthAnimation.To = 35;
+                    broder_s = false;
+                }
+                else
+                {
+                    widthAnimation.From = 35;
+                    widthAnimation.To = aawidth;
+                    broder_s = true;
+                }
+                BorderWindow.BeginAnimation(Border.WidthProperty, widthAnimation);
             }
 
-            BorderWindow.BeginAnimation(Border.WidthProperty, widthAnimation);
+            
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -102,6 +188,10 @@ namespace 灵动窗Framework
             this.Top = 20;
         }
 
-
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            //禁止关闭
+        }
     }
 }
